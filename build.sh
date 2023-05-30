61273513f03bf38174c57d9039bed000c467eebf
Codezigineer <53050996+Codezigineer@users.noreply.github.com>
2023-05-29 22:18:19 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 set -e
 mkdir build
-asc ./src/*.ts --optimizeLevel 3 -o ./build neuraFF.wasm
-tsc ./src/js/*.ts --outDir ./build
+npm run asc ./src/*.ts --optimizeLevel 3 -o ./build neuraFF.wasm
+npm run tsc ./src/js/*.ts --outDir ./build
